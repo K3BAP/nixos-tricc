55504aad16fd07853fa922955e3083e8f5aa4b4e
Sponholz, Fabian <s4faspon@uni-trier.de>
2024-10-13 14:12:31 +0200

    Small fix

--- tricc-user.nix
+++ tricc-user.nix
@@ -6,9 +6,6 @@
     isNormalUser = true;
     description = "Trier Coding Contest 2024";
     extraGroups = [ "networkmanager" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
   };
 
   # Enable automatic login for the user.
